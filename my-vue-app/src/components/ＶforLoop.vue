0a16f33563ff534ca4324378bfd21db53460d9cf
Tingan <syeak123@gmail.com>
2025-02-09 15:43:23 +0800

    fear:add v-for training

---
+++ my-vue-app/src/components/ＶforLoop.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {ref} from 'vue';
+const title=ref('標題')
+const items=ref({
+  1:{id:1,label:'內容一'},
+  2:{id:1,label:'內容二'},
+  3:{id:1,label:'內容三'},
+})
+</script>
+<template>
+<h1>{{title}}</h1>
+<ul>
+  <li v-for="({id,label},index) in items" :key="id">{{index}}{{label}}</li>
+</ul>
+
+</template>
